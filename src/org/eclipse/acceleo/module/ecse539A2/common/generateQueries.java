15eea079fcbe8b725925d4f08a3d3c9158c1d766
Alec Parent <alec.parent8@gmail.com>
2018-10-26 17:21:51 -0400

    It recognizes Actor now

--- src/org/eclipse/acceleo/module/ecse539A2/common/generateQueries.java
+++ src/org/eclipse/acceleo/module/ecse539A2/common/generateQueries.java
@@ -1,9 +1,13 @@
 package org.eclipse.acceleo.module.ecse539A2.common;
 
+import java.util.ArrayList;
+
 import org.eclipse.emf.common.util.EList;
 
 import grl.Actor;
+import grl.ElementLink;
 import grl.GRLspec;
+import grl.IntentionalElement;
 import urn.URNspec;
 
 public class generateQueries {
@@ -22,6 +26,46 @@ public class generateQueries {
 	
 	public String getActorName(Actor a){
 		return a.getName();
+	}
+	
+	//TODO
+	public String getRootName(URNspec u){
+		return "";
+	}
+	
+	//TODO
+	public ArrayList<Integer> getOutgoingLinks(IntentionalElement ie){
+		return new ArrayList<Integer>();
+	}
+	
+	//TODO
+	public int getDestNode(ElementLink l){
+		return 0;
+	}
+	
+	//TODO
+	public String getNodeType(IntentionalElement ie){
+		return "";
+	}
+	
+	//TODO
+	public ArrayList<Integer> getElementsOfActor(Actor a){
+		return new ArrayList<Integer>();
+	}
+	
+	//TODO
+	public String getLinkType(ElementLink l){
+		return "";
+	}
+	
+	//TODO
+	public String getDependencyName(ElementLink l){
+		return "";
+	}
+	
+	//TODO
+	public ArrayList<Integer> getComplimentaryDecompositionLinks(ElementLink l){
+		return new ArrayList<Integer>();
 	}
 	
 }
